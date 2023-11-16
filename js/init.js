952304acd9b33eccb2d691f0f0d096a1d822ebf3
alan-chat43 <150829923+alan-chat43@users.noreply.github.com>
2023-11-16 10:35:21 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+  apiKey: "AIzaSyCnO_CQ9c1vq2WlFEyzBTMlSEtXm2gAp4Q",
+  authDomain: "clonar43ajse.firebaseapp.com",
+  projectId: "clonar43ajse",
+  storageBucket: "clonar43ajse.appspot.com",
+  messagingSenderId: "922859579652",
+  appId: "1:922859579652:web:8e861c600498721433c851",
+  measurementId: "G-L45NC7HY80"
 });
